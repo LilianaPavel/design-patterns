a92364547c599fcc144f2266488a90f0d401211c
LilianaPavel <luchian.liliana.m@gmail.com>
2018-02-20 11:11:19 +0100

    SingletonPattern

---
+++ DesignPatterns/SingletonPattern/ChocolateBoiler.cs
@@ -0,0 +1,90 @@
+﻿using System;
+
+namespace DesignPatterns.SingletonPattern
+{
+    class ChocolateBoiler
+    {
+        private bool empty;
+        private bool boiled;
+
+        private static ChocolateBoiler instance;
+
+        private ChocolateBoiler()
+        {
+            this.empty = true;
+            this.boiled = false;
+        }
+
+        public static ChocolateBoiler Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    instance = new ChocolateBoiler();
+                }
+                return instance;
+            }
+        }
+
+        // With double-check lock
+        /*
+        static object obj = new object();
+        public static ChocolateBoiler Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    lock (obj)
+
+                        if (instance == null)
+                        {
+                            instance = new ChocolateBoiler();
+                        }
+                    }
+                }
+                return instance;
+            }
+        }
+        */
+
+        public void Fill()
+        {
+            if (IsEmpty())
+            {
+                Console.WriteLine("Fill");
+                this.empty = false;
+                this.boiled = false;
+            }
+        }
+
+        public void Drain()
+        {
+            if(!IsEmpty() && IsBoiled())
+            {
+                Console.WriteLine("Drain");
+                this.empty = true;
+            }
+        }
+
+        public void Boil()
+        {
+            if (!IsEmpty() && !IsBoiled())
+            {
+                Console.WriteLine("Boil");
+                boiled = true;
+            }
+        }
+
+        public bool IsEmpty()
+        {
+            return this.empty;
+        }
+
+        public bool IsBoiled()
+        {
+            return this.boiled;
+        }
+    }
+}
---
+++ DesignPatterns/SingletonPattern/ChocolateFactory.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatterns.SingletonPattern
+{
+    class ChocolateFactory
+    {
+        static void Main(string[] args)
+        {
+            ChocolateBoiler boiler = ChocolateBoiler.Instance;
+            boiler.Fill();
+            boiler.Boil();
+            boiler.Drain();
+
+            Console.ReadKey();
+        }
+    }
+}
